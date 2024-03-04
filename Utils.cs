295b3727c840c31206cc38583e5212b2fe50cf1d
Ark223 <Ark223Neww@gmail.com>
2024-03-04 10:49:25 +0100

    Fix spacing

--- Utils.cs
+++ Utils.cs
@@ -5,51 +5,51 @@ namespace BGA
 {
     internal class Utils
     {
-		internal IEnumerable<byte[]> Generate(int n, int k)
-		{
-			byte[] result = new byte[k];
-			var stack = new Stack<byte>();
-			stack.Push(0);
-			while (stack.Count > 0)
-			{
-				int index = stack.Count - 1;
-				byte value = stack.Pop();
-				while (value < n)
-				{
-					result[index++] = ++value;
-					stack.Push(value);
-					if (index == k)
-					{
-						yield return result;
-						break;
-					}
-				}
-			}
-		}
-
-		internal int Count(int n, int k)
+        internal IEnumerable<byte[]> Generate(int n, int k)
         {
-			int result = 1;
-			if (k > n - k) k = n - k;
-			for (int i = 1; i <= k; i++)
+            byte[] result = new byte[k];
+            var stack = new Stack<byte>();
+            stack.Push(0);
+            while (stack.Count > 0)
             {
-				result *= n - k + i;
-				result /= i;
+                int index = stack.Count - 1;
+                byte value = stack.Pop();
+                while (value < n)
+                {
+                    result[index++] = ++value;
+                    stack.Push(value);
+                    if (index == k)
+                    {
+                        yield return result;
+                        break;
+                    }
+                }
             }
-			return result;
         }
 
-		internal void Shuffle(int[] array, int sum, Random random)
-		{
-			int n = sum;
-			while (n > 1)
-			{
-				n--;
-				int k = random.Next(n + 1);
-				int value = array[k];
-				array[k] = array[n];
-				array[n] = value;
-			}
-		}
-	}
+        internal int Count(int n, int k)
+        {
+            int result = 1;
+            if (k > n - k) k = n - k;
+            for (int i = 1; i <= k; i++)
+            {
+                result *= n - k + i;
+                result /= i;
+            }
+            return result;
+        }
+
+        internal void Shuffle(int[] array, int sum, Random random)
+        {
+            int n = sum;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                int value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+        }
+    }
 }
